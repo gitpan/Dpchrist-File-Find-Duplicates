81751d6a935e0aeeac677beb7ce4505e14895e4e
David Paul Christensen <dpchrist@holgerdanske.com>
2010-12-01 20:46:21 +0000

    Import of DPCHRIST/Dpchrist-File-Find-Duplicates-1.015 from CPAN.
    gitpan-cpan-distribution: Dpchrist-File-Find-Duplicates
    gitpan-cpan-version:      1.015
    gitpan-cpan-path:         DPCHRIST/Dpchrist-File-Find-Duplicates-1.015.tar.gz
    gitpan-cpan-author:       DPCHRIST
    gitpan-cpan-maturity:     released

--- lib/Dpchrist/File/Find/Duplicates.pm
+++ lib/Dpchrist/File/Find/Duplicates.pm
@@ -1,5 +1,5 @@
 #######################################################################
-# $Id: Duplicates.pm,v 1.14 2010-11-27 07:37:15 dpchrist Exp $
+# $Id: Duplicates.pm,v 1.15 2010-11-30 20:20:14 dpchrist Exp $
 #######################################################################
 # package:
 #----------------------------------------------------------------------
@@ -21,7 +21,7 @@ our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
 
 our @EXPORT = qw();
 
-our $VERSION = sprintf("%d.%03d", q$Revision: 1.14 $ =~ /(\d+)/g);
+our $VERSION = sprintf("%d.%03d", q$Revision: 1.15 $ =~ /(\d+)/g);
 
 #######################################################################
 # uses:
@@ -105,7 +105,7 @@ Dpchrist::File::Find::Duplicates - find duplicate files
 
 =head1 DESCRIPTION
 
-This documentation describes module revision $Revision: 1.14 $.
+This documentation describes module revision $Revision: 1.15 $.
 
 
 This is alpha test level software
